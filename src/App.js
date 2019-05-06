783043f5ba573bc0f4ab8f6ab7669cafe1f9c024
Carl Cody <CarlCody34@gmail.com>
2019-05-06 15:54:28 -0400

    still not finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import Login from './components/user/Login';
@@ -13,18 +13,16 @@ import WidgetList from "./components/widget/WidgetList";
 import WidgetChooser from "./components/widget/WidgetChooser";
 // import WebsiteNew from "./components/website/WebsiteNew";
 
-function App() {
-
-  const users= [
+class App extends Component() {
+state = {
+   users: [
       {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder", email: "alice@gmail.com"},
       {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley", email: "bob@whatever.com"},
       {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia", email: "charly@ulem.com"},
       {_id: "456", username: "shiyu", password: "shiyu", firstName: "Shiyu", lastName: "Wang", email: "swang@ulem.org"}
-      ]
-      
-  
-
-  const websites= [
+      ],
+    
+   websites: [
     { _id: "123", name: "Facebook", developerId: "456", description: "Lorem" },
     { _id: "234", name: "Tweeter",  developerId: "456", description: "Lorem" },
     { _id: "456", name: "Gizmodo",   developerId: "456", description: "Lorem" },
@@ -32,33 +30,35 @@ function App() {
     { _id: "567", name: "Tic Tac Toe", developerId: "123", description: "Lorem" },
     { _id: "678", name: "Checkers", developerId: "123", description: "Lorem" },
     { _id: "789", name: "Chess", developerId: "234", description: "Lorem" }
-  ]
+  ],
 
-  const pages= [
+   pages: [
     { _id: "321", name: "Post 1", websiteId: "456", title: "Lorem" },
     { _id: "432", name: "Post 2", websiteId: "456", title: "Lorem" },
     { _id: "543", name: "Post 3", websiteId: "456", title: "Lorem" }
-  ]
+  ],
 
-  const widgets=[
+   widgets:[
     { _id: "123", widgetType: "HEADING", pageId: "321", size: 2, text: "GIZMODO"},
     { _id: "234", widgetType: "HEADING", pageId: "321", size: 4, text: "Lorem ipsum"},
     { _id: "345", widgetType: "IMAGE", pageId: "321", width: "100%", url: "https://www.gettyimages.ie/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg"},
     { _id: "567", widgetType: "HEADING", pageId: "321", size: 4, text: "Lorem ipsum"},
     { _id: "678", widgetType: "YOUTUBE", pageId: "321", width: "100%", url: "https://youtu.be/AM2Ivdi9c4E" },
-  ]
-   
-  
-  
+  ],
+} 
 
+
+ render () {
+   
   return (
   <Router>
-    <Route exact path="/" component={Login}></Route>
-    <Route exact path="/profile" component={Profile}></Route>
-    <Route exact path="/login" component={Login}></Route>
-    <Route exact path="/register" component={Register}></Route>
-    <Route exact path="/user/:uid" component={Profile}></Route>
-    <Route exact path="/user/:uid/website" component={WebsiteList}></Route>
+    <Route exact path="/" render={props => (<Login {...props} users={this.state.users}/>)} />
+    <Route exact path="/" render={props => (<Login {...props} users={this.state.users}/>)} />
+    <Route exact path="/profile" render={props => (<Profile {...props} users={this.state.users}/>)} />>
+    <Route exact path="/login" render={props => (<Login {...props} users={this.state.users}/>)} />
+    <Route exact path="/register"render={props => (<Register {...props} users={this.state.users}/>)} />
+    <Route exact path="/user/:uid" render={props => (<Profile {...props} users={this.state.users}/>)} />
+    <Route exact path="/user/:uid/website" render={props => (<WebsiteList {...props} users={this.state.users}/>)} />
     {/* <Route exact path="/user/:uid/website/new" component={WebsiteNew}></Route> */}
     <Route exact path="/user/:uid/website/:wid" component={WebsiteEdit}></Route>
     <Route exact path="/user/:uid/website/:wid/page" component={PageList}></Route>
@@ -69,7 +69,9 @@ function App() {
     {/* <Route exact path="/user/:uid/website/:wid/page/new/:pid/widget/:wgid" component={WidgetEdit}></Route> */}
   </Router>
   )
+} 
 }
+
 
 // class act extends Components {
 
--- user/Login.js
+++ user/Login.js
@@ -6,19 +6,31 @@ export default class Login extends React.Component {
 //      username: "Carl",
 //       password: "carl123"
 //  }
-
+onChange = e => {
+    if(e.target.name === "username")
+    this.setState({
+      username: e.target.value
+    })
+} else {
+    this.setState({
+        password: e.target.value
+    })
+}
+onSubmit = e => {
+    e.preventDefault();
+}
 render(){
     return (
         <div className="container">
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.onSubmit}>
             <div className="form-group">
                 <label htmlFor="username">Username</label>
                 <input placeholder="Enter your Name" className="form-control" type="text" name="username" id="username"/>
             </div>
             <div className="form-group">
                 <label htmlFor="password">Password</label>
-                <input placeholder="Enter your password" type="password" className="form-control" id="passwod" name="password"/>
+                <input placeholder="Enter your password" type="password" className="form-control" id="passwod" name="password" value={this.state.password} onchange = {this.onChange}/>
             </div>
             <a className="btn btn-success btn-block" href="profile.html">Login</a>
             <a className="btn btn-primary btn-block" href="register.html">Register</a>
