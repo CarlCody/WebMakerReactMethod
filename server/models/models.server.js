a2f78d7490905e8a8c64dc122eece75db7980e08
Carl Cody <CarlCody34@gmail.com>
2019-06-07 13:56:06 -0400

    fix connection string

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -5,7 +5,7 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = 'mongodb://' + username + ':' + password;
-  connectionString += 'ds135207.mlab.com:35207/heroku_72rkr1tw'; // use yours
+  connectionString += '@ds135207.mlab.com:35207/heroku_72rkr1tw'; // use yours
 }
 
 mongodb://<dbuser>:<dbpassword>@ds135207.mlab.com:35207/heroku_72rkr1tw
